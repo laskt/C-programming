b4b68dfaf5f7c87953e853b0c95c3940ac0ad8cf
Coursera Learner <nobody@nowhere.nul>
2018-01-15 23:29:08 +0000

    Did assignment 4

--- 03_code2/code2.c
+++ 03_code2/code2.c
@@ -17,7 +17,7 @@ int main(void){
   printf("Here is a triangle with height 4\n");
   numStars = printTriangle(4);
   printf("That triangle had %d total stars\n", numStars);
-  printf("Here is a trinagle with height 7\n");//now print "Here is a triangle with height 7\n"
+  printf("Here is a triangle with height 7\n");//now print "Here is a triangle with height 7\n"
   numStars = printTriangle(7);//then call printTriangle, passing in 7, and assign the result to numStars
   printf("That triangle had %d total stars\n", numStars);//finally, print "That triangle had %d total stars\n", such that the %d //prints the value of numStars
   return 0;
