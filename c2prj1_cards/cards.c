75f50b9d7ddd463baabfd02078015e730f8e851e
Coursera Learner <nobody@nowhere.nul>
2018-02-12 22:39:45 +0000

    Added cards.c final

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -10,15 +10,15 @@ void assert_card_valid(card_t c) {
 
 const char * ranking_to_string(hand_ranking_t r) {
   switch(r){
-  case STRAIGHT_FLUSH : return "One\n";break;
-  case FOUR_OF_A_KIND : return "Two\n";break;
-  case FULL_HOUSE : return "Three\n";break;
-  case FLUSH : return "Four\n";break;
-  case STRAIGHT : return "Five\n";break;
-  case THREE_OF_A_KIND : return "Six\n";break;
-  case TWO_PAIR : return "Seven\n";break;
-  case PAIR : return "Eight\n";break;
-  case NOTHING : return "Nine\n";break;
+  case STRAIGHT_FLUSH : return "Zero\n";break;
+  case FOUR_OF_A_KIND : return "One\n";break;
+  case FULL_HOUSE : return "Two\n";break;
+  case FLUSH : return "Three\n";break;
+  case STRAIGHT : return "Four\n";break;
+  case THREE_OF_A_KIND : return "Five\n";break;
+  case TWO_PAIR : return "Six\n";break;
+  case PAIR : return "Seven\n";break;
+  case NOTHING : return "Eight\n";break;
   default: return EXIT_SUCCESS;break;
   }
 }
